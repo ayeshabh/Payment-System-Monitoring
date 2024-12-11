62726159bd116c35a5e25a9eaaa3ec7d3cebe626
Ayesha Bhangu <33913171+ayeshabh@users.noreply.github.com>
2024-12-11 02:29:49 -0500

    Update create_schema.sql
    updated the create schema sql code

--- sql-scripts/create_schema.sql
+++ sql-scripts/create_schema.sql
@@ -1,1 +1,8 @@
-ok 
+-- Create the transactions table
+CREATE TABLE transactions (
+    transaction_id SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    amount DECIMAL(10, 2),
+    status VARCHAR(10) CHECK (status IN ('success', 'failure', 'pending')),
+    failure_reason VARCHAR(255)
+);
